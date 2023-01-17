86cb018081cbf9ec53c338522d88b99d62da729a
warryy <warryy@qq.com>
2023-01-17 15:08:20 +0800

    feat: temp

--- src/02-advanced/01-type-checking-mechanism.ts
+++ src/02-advanced/01-type-checking-mechanism.ts
@@ -3,7 +3,7 @@
  *      类型检查机制
  *          表达式从右向左推断
  *          事件处理会发生从左向右推断
- *      兼容性总结: 
+ *      兼容性总结:
  *          定义: 当 y 可以给 x 赋值时, 我们说 x 兼容 y 类型
  *          结论: 函数参数多的兼容参数少的, 其余的属性少的兼容属性多的
  */
@@ -27,12 +27,12 @@ let arr = [1, null];
 window.onkeydown = (event) => {
   // 这里的 event 会被推断成 onkeydown 事件的参数类型: KeyboardEvent
 };
-window.addEventListener('test', (e) => {
-    // 这里的 e 也会被推断出来
-})
+window.addEventListener("test", (e) => {
+  // 这里的 e 也会被推断出来
+});
 
 /**
- * 当 y 可以给 x 赋值是, 我们说 x 兼容 y 类型
+ * 当 y 可以给 x 赋值时, 即 x = y, 我们说 x 兼容 y 类型
  * 接口 X 兼容 Y, 因为 Y 里面有 X 必须的所有属性
  * 即: 属性少的接口兼容属性多的接口
  */
@@ -46,11 +46,16 @@ interface Y {
 
 let x: X = { a: "a" };
 let y: Y = { a: "a", b: "b" };
-x = y; 
+x = y;
 // y = x; // 类型 "Y" 中缺少属性 "b"，但类型 "X" 中需要该属性。
 
 /**
  * 类型兼容性: 函数
+ * a. 函数参数的个数, 参数多的函数兼容参数少的函数
+ *   个人理解: 假如下方 hof 函数的参数 h 是求两数之积,
+ *     h1 函数传入一个数字, 结果会报错, 此时可以通过报错排查到问题
+ *     h3 函数传入三个数字 , 结果不会报错, 但是返回的数据是其中两个数字的积, 对于传入 h3 的使用者来说, 不符合预期, 因为他想计算的是三数之积, 这种问题不好排查
+ *
  */
 type Handler = (a: number, b: number) => void;
 
@@ -58,10 +63,6 @@ function hof(h: Handler) {
   return h;
 }
 
-/**
- * 类型兼容性: 函数
- * a. 函数参数的个数
- */
 let h1 = (a: number) => {};
 let h2 = (a: number, b: number, c: number) => {};
 hof(h1);
@@ -92,34 +93,34 @@ c = b;
  *      和接口正好相反,参数属性多的函数兼容参数属性少的函数
  */
 type Point3D = {
-    x: number
-    y: number
-    z: number
-}
+  x: number;
+  y: number;
+  z: number;
+};
 type Point2D = {
-    x: number
-    y: number
-}
-let f3d = (p: Point3D) => {}
-let f2d = (p: Point2D) => {}
+  x: number;
+  y: number;
+};
+let f3d = (p: Point3D) => {};
+let f2d = (p: Point2D) => {};
 // f2d = f3d // error
-f3d = f2d
+f3d = f2d;
 
 /**
  * 类型兼容性: 函数
  *      返回值属性少的函数兼容返回值属性多的函数
  */
-let f1 = (): {x: number, y: number} => ({x: 1, y:2})
-let f2 = (): {x: number} => ({x: 1})
+let f1 = (): { x: number; y: number } => ({ x: 1, y: 2 });
+let f2 = (): { x: number } => ({ x: 1 });
 // f1 = f2 // error
-f2 = f1
+f2 = f1;
 
 /**
  * 类型兼容性: 函数
  *      实现重载的函数要是最宽泛版本, 即入参个数可以少, 但是不能多
  */
-function overload(a: number, b: number): number
-function overload(a: any):any {}
+function overload(a: number, b: number): number;
+function overload(a: any): any {}
 // function overload(a: any, b: any, c:any):any {} // error
 
 /**
@@ -127,63 +128,61 @@ function overload(a: any):any {}
  *      a. number 和 数字枚举互相兼容, 枚举之间互不兼容
  */
 enum EFruit {
-    Apple,
+  Apple,
 }
-let apple: EFruit.Apple = 3
-let num: number = EFruit.Apple
+let apple: EFruit.Apple = 3;
+let num: number = EFruit.Apple;
 
 /**
- * 类型兼容: 类  
- *      a. 类只比较结构, 静态成员和构造函数不参与比较 
+ * 类型兼容: 类
+ *      a. 类只比较结构, 静态成员和构造函数不参与比较
  *      b. 父类和子类的实例可以完全兼容
  */
 class A {
-    static sta: 1
-    constructor(a: number, b: string){}
-    id: number = 1
-    // private pri = 2
-    // protected pro = 2
+  static sta: 1;
+  constructor(a: number, b: string) {}
+  id: number = 1;
+  // private pri = 2
+  // protected pro = 2
 }
 class B {
-    constructor(a: number){}
-    id: number = 2
+  constructor(a: number) {}
+  id: number = 2;
 }
 
-let aa = new A(1, '2')
-let bb = new B(1)
+let aa = new A(1, "2");
+let bb = new B(1);
 
-aa = bb
-bb = aa
+aa = bb;
+bb = aa;
 
 class C extends A {}
-let cc = new C(1, '2')
-cc = aa
-aa = cc
+let cc = new C(1, "2");
+cc = aa;
+aa = cc;
 
 /**
  * 类型兼容: 泛型接口
  *      只有泛型 T, 被接口成员使用的时候, 才会出现泛型互不兼容
  */
 interface EmptyObj<T> {
-    // value?: T
+  // value?: T
 }
-let obj1: EmptyObj<string> = {}
-let obj2: EmptyObj<number> = {}
-obj1 = obj2
-obj2 = obj1
+let obj1: EmptyObj<string> = {};
+let obj2: EmptyObj<number> = {};
+obj1 = obj2;
+obj2 = obj1;
 
 /**
  * 类型兼容: 泛型函数
  */
 let log1 = <T>(x: T): T => {
-    return x
-}
+  return x;
+};
 let log2 = <U>(x: U): U => {
-    return x
-}
-log1 = log2
-log2 = log1
+  return x;
+};
+log1 = log2;
+log2 = log1;
 
-
-
-export {}
+export {};
--- src/08-tsconfig-json-compile-opt/src/index.ts
+++ src/08-tsconfig-json-compile-opt/src/index.ts
@@ -1,13 +1,23 @@
 // import AMDVarA = require('./amd')
-console.log('I\'m index file')
+console.log("I'm index file");
 
 // // es2019
 // console.log([1,2, [3]].flat())
 
-class A {}
+class A {
+  constructor() {}
+  name: string
+}
 
+// 这是一行注释
+/** 这是一行注释2 */
 class B extends A {}
 
-export = B
+export = B;
 
-const a = [1,2, ...[3]]
+const a = [1, 2, ...[3]];
+
+// noImplicitlyAny: false, 此时函数参数可以不去指定类型
+// function fn(arg) {
+//     console.log(arg)
+// }
