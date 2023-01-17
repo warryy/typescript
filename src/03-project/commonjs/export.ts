f235b3975ee6da3a5a030e82e689dd10a22c12bd
wangyanyan.warryy <wangyanyan.warryy@bytedance.com>
2023-01-18 03:57:01 +0800

    feat: 工程打包相关配置

--- src/03-project/commonjs/export.ts
+++ src/03-project/commonjs/export.ts
@@ -1,8 +1,9 @@
 /**
  * commonjs 导出
  *      exports === module.exports
+ *          exports = {} 或 module.exports = {}
+ *          如果文件中加入上面这行代码, 会将 exports.xxx 全部覆盖掉
  */ 
-exports = {}
 
 exports.a = 'a'
 exports.b = 'b'
--- src/08-tsconfig-json-compile-opt/src/index.ts
+++ src/08-tsconfig-json-compile-opt/src/index.ts
@@ -4,20 +4,43 @@ console.log("I'm index file");
 // // es2019
 // console.log([1,2, [3]].flat())
 
-class A {
-  constructor() {}
-  name: string
-}
+// class A {
+//   constructor() {}
+//   name: string;
+// }
 
-// 这是一行注释
-/** 这是一行注释2 */
-class B extends A {}
+// // 这是一行注释
+// /** 这是一行注释2 */
+// class B extends A {}
 
-export = B;
+// export = B;
 
-const a = [1, 2, ...[3]];
+// const a = [1, 2, ...[3]];
 
 // noImplicitlyAny: false, 此时函数参数可以不去指定类型
 // function fn(arg) {
 //     console.log(arg)
 // }
+
+// /** noImplicitThis */
+// class NoImplicitThis {
+//   a = 1;
+//   getA() {
+//     return function () {
+//       console.log(this.a);
+//     };
+//   }
+// }
+
+/**
+ * @noFallthroughCasesInSwitch
+ * @errors: 7029
+ */ 
+const a: number = 6;
+switch (a) {
+  case 0:
+    console.log("even");
+  case 1:
+    console.log("odd");
+    break;
+}
